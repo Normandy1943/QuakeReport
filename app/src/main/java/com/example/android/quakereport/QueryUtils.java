8213ba7cfc309dbbf75c2e45e3c4d14e42d83fa2
Normandy1943 <normandy1943@sina.cn>
2017-07-20 13:01:58 +0800

    网络请求 数据解析

--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -1,19 +1,29 @@
 package com.example.android.quakereport;
 
+import android.text.TextUtils;
 import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Helper methods related to requesting and receiving earthquake data from USGS.
  */
 public final class QueryUtils {
 
-    /** Sample JSON response for a USGS query */
+   /* *//** Sample JSON response for a USGS query *//*
     private static final String SAMPLE_JSON_RESPONSE =
             "{\"type\":\"FeatureCollection\",\"metadata\":{\"generated\":1462295443000,\"url\":\"http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-01-31&minmag=6&limit=10\",\"title\":\"USGS Earthquakes\",\"status\":200,\"api\":\"1.5.2\",\"limit\":10,\"offset\":1,\"count\":10},\"features\":[{\"type\":\"Feature\",\"properties\":{\"mag\":0.2,\"place\":\"88km N of Yelizovo, Russia\",\"time\":1454124312220,\"updated\":1460674294040,\"tz\":720,\"url\":\"http://earthquake.usgs.gov/earthquakes/eventpage/us20004vvx\",\"detail\":\"http://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us20004vvx&format=geojson\",\"felt\":2,\"cdi\":3.4,\"mmi\":5.82,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":1,\"sig\":798,\"net\":\"us\",\"code\":\"20004vvx\",\"ids\":\",at00o1qxho,pt16030050,us20004vvx,gcmt20160130032510,\",\"sources\":\",at,pt,us,gcmt,\",\"types\":\",cap,dyfi,finite-fault,general-link,general-text,geoserve,impact-link,impact-text,losspager,moment-tensor,nearby-cities,origin,phase-data,shakemap,tectonic-summary,\",\"nst\":null,\"dmin\":0.958,\"rms\":1.19,\"gap\":17,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 7.2 - 88km N of Yelizovo, Russia\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[158.5463,53.9776,177]},\"id\":\"us20004vvx\"},\n" +
                     "{\"type\":\"Feature\",\"properties\":{\"mag\":1.1,\"place\":\"94km SSE of Taron, Papua New Guinea\",\"time\":1453777820750,\"updated\":1460156775040,\"tz\":600,\"url\":\"http://earthquake.usgs.gov/earthquakes/eventpage/us20004uks\",\"detail\":\"http://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us20004uks&format=geojson\",\"felt\":null,\"cdi\":null,\"mmi\":4.1,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":1,\"sig\":572,\"net\":\"us\",\"code\":\"20004uks\",\"ids\":\",us20004uks,gcmt20160126031023,\",\"sources\":\",us,gcmt,\",\"types\":\",cap,geoserve,losspager,moment-tensor,nearby-cities,origin,phase-data,shakemap,tectonic-summary,\",\"nst\":null,\"dmin\":1.537,\"rms\":0.74,\"gap\":25,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 6.1 - 94km SSE of Taron, Papua New Guinea\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[153.2454,-5.2952,26]},\"id\":\"us20004uks\"},\n" +
@@ -25,18 +35,18 @@ public final class QueryUtils {
                     "{\"type\":\"Feature\",\"properties\":{\"mag\":7.2,\"place\":\"74km NW of Rumoi, Japan\",\"time\":1452532083920,\"updated\":1459304875040,\"tz\":540,\"url\":\"http://earthquake.usgs.gov/earthquakes/eventpage/us10004djn\",\"detail\":\"http://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us10004djn&format=geojson\",\"felt\":8,\"cdi\":3.4,\"mmi\":3.74,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":0,\"sig\":594,\"net\":\"us\",\"code\":\"10004djn\",\"ids\":\",us10004djn,gcmt20160111170803,\",\"sources\":\",us,gcmt,\",\"types\":\",cap,dyfi,geoserve,impact-text,losspager,moment-tensor,nearby-cities,origin,phase-data,shakemap,tectonic-summary,\",\"nst\":null,\"dmin\":1.139,\"rms\":0.96,\"gap\":33,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 6.2 - 74km NW of Rumoi, Japan\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[141.0867,44.4761,238.81]},\"id\":\"us10004djn\"},\n" +
                     "{\"type\":\"Feature\",\"properties\":{\"mag\":8.5,\"place\":\"227km SE of Sarangani, Philippines\",\"time\":1452530285900,\"updated\":1459304874040,\"tz\":480,\"url\":\"http://earthquake.usgs.gov/earthquakes/eventpage/us10004dj5\",\"detail\":\"http://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us10004dj5&format=geojson\",\"felt\":1,\"cdi\":2.7,\"mmi\":7.5,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":1,\"sig\":650,\"net\":\"us\",\"code\":\"10004dj5\",\"ids\":\",at00o0srjp,pt16011050,us10004dj5,gcmt20160111163807,\",\"sources\":\",at,pt,us,gcmt,\",\"types\":\",cap,dyfi,geoserve,impact-link,impact-text,losspager,moment-tensor,nearby-cities,origin,phase-data,shakemap,tectonic-summary,\",\"nst\":null,\"dmin\":3.144,\"rms\":0.72,\"gap\":22,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 6.5 - 227km SE of Sarangani, Philippines\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[126.8621,3.8965,13]},\"id\":\"us10004dj5\"},\n" +
                     "{\"type\":\"Feature\",\"properties\":{\"mag\":9,\"place\":\"Pacific-Antarctic Ridge\",\"time\":1451986454620,\"updated\":1459202978040,\"tz\":-540,\"url\":\"http://earthquake.usgs.gov/earthquakes/eventpage/us10004bgk\",\"detail\":\"http://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us10004bgk&format=geojson\",\"felt\":0,\"cdi\":1,\"mmi\":0,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":0,\"sig\":554,\"net\":\"us\",\"code\":\"10004bgk\",\"ids\":\",us10004bgk,gcmt20160105093415,\",\"sources\":\",us,gcmt,\",\"types\":\",cap,dyfi,geoserve,losspager,moment-tensor,nearby-cities,origin,phase-data,shakemap,\",\"nst\":null,\"dmin\":30.75,\"rms\":0.67,\"gap\":71,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 6.0 - Pacific-Antarctic Ridge\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[-136.2603,-54.2906,10]},\"id\":\"us10004bgk\"}],\"bbox\":[-153.4051,-54.2906,10,158.5463,59.6363,582.56]}";
-    /**
+    *//**
      * Create a private constructor because no one should ever create a {@link QueryUtils} object.
      * This class is only meant to hold static variables and methods, which can be accessed
      * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
-     */
+     *//*
     private QueryUtils() {
     }
 
-    /**
+    *//**
      * Return a list of {@link Earthquake} objects that has been built up from
      * parsing a JSON response.
-     */
+     *//*
     public static ArrayList<Earthquake> extractEarthquakes() {
 
         // Create an empty ArrayList that we can start adding earthquakes to
@@ -76,6 +86,176 @@ public final class QueryUtils {
         }
 
         // Return the list of earthquakes
+        return earthquakes;
+    }*/
+
+    /**
+     * Tag for the log messages
+     */
+    public static final String LOG_TAG = QueryUtils.class.getSimpleName();
+
+    /**
+     * 查询 USGS 数据集并返回 {@link Earthquake} 对象的列表。
+     */
+    public static List<Earthquake> fetchEarthquakeData(String requestUrl) {
+        // 创建 URL 对象
+        URL url = createUrl(requestUrl);
+
+        // 执行 URL 的 HTTP 请求并接收返回的 JSON 响应
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
+        }
+
+        // 从 JSON 响应提取相关域并创建 {@link Earthquake} 的列表
+        List<Earthquake> earthquakes = extractFeatureFromJson(jsonResponse);
+
+        // 返回 {@link Earthquake} 的列表
+        return earthquakes;
+    }
+
+
+    /**
+     * 从给定字符串 URL 返回新 URL 对象。
+     */
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Problem building the URL ", e);
+        }
+        return url;
+    }
+
+    /**
+     * 向给定 URL 进行 HTTP 请求，并返回字符串作为响应。
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        // 如果 URL 为空，则提早返回。
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // 如果请求成功（响应代码 200），
+            // 则读取输入流并解析响应。
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // 关闭输入流可能会抛出 IOException，这就是
+                //  makeHttpRequest(URL url) 方法签名指定可能抛出 IOException 的
+                // 原因。
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    /**
+     * 将 {@link InputStream} 转换为包含
+     * 来自服务器的整个 JSON 响应的字符串。
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    /**
+     * 返回通过解析给定 JSON 响应构建的 {@link Earthquake} 对象
+     * 列表。
+     */
+    private static List<Earthquake> extractFeatureFromJson(String earthquakeJSON) {
+        // 如果 JSON 字符串为空或 null，将提早返回。
+        if (TextUtils.isEmpty(earthquakeJSON)) {
+            return null;
+        }
+
+        // 创建一个可以添加地震的空 ArrayList
+        List<Earthquake> earthquakes = new ArrayList<>();
+
+        // 尝试解析 JSON 响应字符串。如果格式化 JSON 的方式存在问题，
+        // 则将抛出 JSONException 异常对象。
+        // 捕获该异常以便应用不会崩溃，并将错误消息打印到日志中。
+        try {
+
+            // 通过 JSON 响应字符串创建 JSONObject
+            JSONObject baseJsonResponse = new JSONObject(earthquakeJSON);
+
+            // 提取与名为 "features" 的键关联的 JSONArray，
+            // 该键表示特征（或地震）列表。
+            JSONArray earthquakeArray = baseJsonResponse.getJSONArray("features");
+
+            // 针对 earthquakeArray 中的每个地震，创建 {@link Earthquake} 对象
+            for (int i = 0; i < earthquakeArray.length(); i++) {
+
+                // 获取地震列表中位置 i 处的单一地震
+                JSONObject currentEarthquake = earthquakeArray.getJSONObject(i);
+
+                // 针对给定地震，提取与名为 "properties" 的键关联的 JSONObject，
+                // 该键表示该地震所有属性的
+                // 列表。
+                JSONObject properties = currentEarthquake.getJSONObject("properties");
+
+                // 提取名为 "mag" 的键的值
+                double magnitude = properties.getDouble("mag");
+
+                // 提取名为 "place" 的键的值
+                String location = properties.getString("place");
+
+                // 提取名为 "time" 的键的值
+                long time = properties.getLong("time");
+
+                // 提取名为 "url" 的键的值
+                String url = properties.getString("url");
+
+                // 使用 JSON 响应中的震级、位置、时间和 url，
+                // 创建新的 {@link Earthquake} 对象。
+                Earthquake earthquake = new Earthquake(magnitude, location, time, url);
+
+                // 将该新 {@link Earthquake} 添加到地震列表。
+                earthquakes.add(earthquake);
+            }
+
+        } catch (JSONException e) {
+            // 在 "try" 块中执行上述任一语句时若系统抛出错误，
+            // 则在此处捕获异常，以便应用不会崩溃。在日志消息中打印
+            // 来自异常的消息。
+            Log.e("QueryUtils", "Problem parsing the earthquake JSON results", e);
+        }
+
+        // 返回地震列表
         return earthquakes;
     }
 
